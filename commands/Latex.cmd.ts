8d72bf8351c2c4a0c91228f94193445fadba42f0
Hammad Izhar <hammad_izhar@brown.edu>
2022-01-04 16:32:59 -0500

    Latex Support Added

--- commands/Latex.cmd.ts
+++ commands/Latex.cmd.ts
@@ -1,24 +1,14 @@
 import { CommandInteraction } from "discord.js";
 import { Discord, SimpleCommand, SimpleCommandMessage, SimpleCommandOption, Slash, SlashOption } from "discordx";
-// import fetch from "node-fetch";
 
 @Discord()
 export abstract class Latex {
-    // @Slash("latex", { description: "Format nice LaTeX equations!" })
-    // private async latex(
-    //     @SlashOption("eqn", { description: "The LaTeX equation to render" }) eqn: string,
-    //     ctx: CommandInteraction): Promise<void> {
-    //     console.log(eqn);
-    //     // const response = await fetch(`https://chart.apis.google.com/chart?cht=tx&chl=${eqn}`);
-    //     // console.log(response);
-    // }
-
-    @SimpleCommand("latex", {})
+    @Slash("latex", { description: "Format nice LaTeX equations!" })
     private async latex(
-        @SimpleCommandOption("eqn", { type: "STRING" }) eqn: string | undefined,
-        ctx: SimpleCommandMessage) {
-        if (!eqn) return ctx.message.reply("Please supply an equation!");
+        @SlashOption("eqn", { description: "The LaTeX equation to render" }) eqn: string,
+        ctx: CommandInteraction): Promise<void> {
+        const apiURL = "https://latex.codecogs.com/png.image?";
 
-        ctx.message.reply(eqn);
+        ctx.reply(`${apiURL}${encodeURIComponent("\\color{white}" + eqn)}`);
     }
 }
